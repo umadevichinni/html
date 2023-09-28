c43cca355e562e027d2e50fba6ef75a4009a6a73
umadevichinni <144320493+umadevichinni@users.noreply.github.com>
2023-09-28 15:18:21 +0530

    Update uma.py

--- uma.py
+++ uma.py
@@ -2,9 +2,9 @@
 
 num1 = 1.5
 num2 = 6.3
-
+num3 = 7.5
 # Add two numbers
 sum = num1 + num2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, num3, sum))
